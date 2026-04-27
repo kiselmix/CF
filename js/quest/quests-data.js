d7c541b96415db4028738347c4a1c9f5c6e251f7
Mikhail Kiselev <54370796+kiselmix@users.noreply.github.com>
2026-04-27 17:28:30 +0500

    Add files via upload

--- js/quest/quests-data.js
+++ js/quest/quests-data.js
@@ -4,4 +4,9 @@ window.CRYSTALFALL_QUESTS = [
   { slug: "the-breach-engine-reforged", title: "The Breach Engine Reforged" },
   { slug: "caught-in-a-web", title: "Caught in a Web" },
   { slug: "free-alden", title: "Free Alden" },
+  { slug: "capital-arcadia", title: "Capital Arcadia" },
+  { slug: "commissioner", title: "Commissioner" },
+  { slug: "forceful-withdrawal", title: "Forceful Withdrawal" },
+  { slug: "tools-of-the-trade", title: "Tools of the Trade" },
+  { slug: "tumbleweed-express", title: "Tumbleweed Express" },
 ];
